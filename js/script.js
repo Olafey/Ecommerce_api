8899089d3e7f92750f04a35bf11a41e6e2c67a92
Olafey <olamidearemu2000@yahoo.com>
2024-10-19 18:11:28 +0100

    html cont transferred to js

--- js/script.js
+++ js/script.js
@@ -5,3 +5,16 @@ const apiUrl = async () => {
   console.log(data);
 };
 apiUrl();
+
+let product_html = `
+   <div class="product">
+          <img src="" alt="" class="product_img" />
+          <h2 class="product_title"></h2>
+          <h4 class="product_category"></h4>
+          <p class="product_description"></p>
+          <div class="product_price_container">
+            <h3 class="product_price"></h3>
+            <a href="" class="add_to_cart"></a>
+          </div>
+        </div>
+`;
