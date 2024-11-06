29c905cdbd0250a769240aa49712aa8e92789c3a
Olafey <olamidearemu2000@yahoo.com>
2024-11-06 10:18:05 +0100

    styling update

--- js/script.js
+++ js/script.js
@@ -6,12 +6,20 @@ const apiUrl = async () => {
   const data = await response.json();
 
   for (let i = 0; i < data.length; i++) {
+    let description = data[i].description;
+    let title = data[i].title;
     products.innerHTML += `
    <div class="product">
           <img src="${data[i].image}" alt="PRODUCT IMAGE" class="product_img" />
-          <h2 class="product_title">${data[i].title}</h2>
+          <h2 class="product_title">${
+            title.length > 25 ? title.substring(0, 25).concat(" ... ") : title
+          }</h2>
           <h4 class="product_category">${data[i].category}</h4>
-          <p class="product_description">${data[i].description}</p>
+          <p class="product_description">${
+            description.length > 80
+              ? description.substring(0, 80).concat(" .... more")
+              : description
+          }</p>
           <div class="product_price_container">
             <h3 class="product_price"> $${data[i].price}</h3>
             <a href="#!" class="add_to_cart">${data[i].id}</a>
